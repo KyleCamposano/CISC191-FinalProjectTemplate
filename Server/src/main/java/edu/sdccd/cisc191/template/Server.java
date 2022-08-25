1ad4204b2a5ad1faae33bf8cbef65ae6e957c556
Kyle <kyleralozo126@gmail.com>
2022-08-24 18:29:20 -0700

    Added Module 1.

--- Server/src/main/java/edu/sdccd/cisc191/template/Server.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/Server.java
@@ -1,52 +1,48 @@
 package edu.sdccd.cisc191.template;
 
-import java.net.*;
-import java.io.*;
+//import java.net.*;
+//import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
- * This program is a server that takes connection requests on
- * the port specified by the constant LISTENING_PORT.  When a
- * connection is opened, the program sends the current time to
- * the connected socket.  The program will continue to receive
- * and process connections until it is killed (by a CONTROL-C,
- * for example).  Note that this server processes each connection
- * as it is received, rather than creating a separate thread
- * to process the connection.
+
  */
 public class Server {
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
-
-    public void start(int port) throws Exception {
-        serverSocket = new ServerSocket(port);
-        clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-        String inputLine;
-        while ((inputLine = in.readLine()) != null) {
-            CustomerRequest request = CustomerRequest.fromJSON(inputLine);
-            CustomerResponse response = new CustomerResponse(request.getId(), "Jane", "Doe");
-            out.println(CustomerResponse.toJSON(response));
-        }
-    }
-
-    public void stop() throws IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
-        serverSocket.close();
-    }
-
     public static void main(String[] args) {
-        Server server = new Server();
-        try {
-            server.start(4444);
-            server.stop();
-        } catch(Exception e) {
-            e.printStackTrace();
+        Scanner userInput = new Scanner(System.in); //declaring and initializing Scanner class
+        System.out.println("What color am I thinking?");
+        String userGuess = userInput.nextLine().toLowerCase(); //storing any user input as lowercase
+        //System.out.println(userGuess);
+        int guessLimit = 0;
+
+        String[] colors = {"Red", "Orange", "Yellow", "Green", "Indigo", "Blue", "Violet"};
+        Random random = new Random(); //declaring and initializing Random class
+        int randomColorIndex = random.nextInt(colors.length); //stores the randomly selected index of the array
+        String trueColor = colors[randomColorIndex].toLowerCase(); //stores the color of the randomly selected index
+
+        //System.out.println(trueColor);
+        //This is to manually print out trueColor test whether array and if statements are working together.
+
+        //this while loop continues to prompt the user until the guessLimit is reached, and ends when
+        //guessLimit is reached (or the user correctly guesses!).
+        while (!userGuess.equals(trueColor) && guessLimit < 2) {
+            System.out.println("Sorry, wrong color!");
+            guessLimit += 1;
+            userGuess = userInput.nextLine().toLowerCase();
+            if (!userGuess.equals(trueColor) && guessLimit == 2) {
+                System.out.println("Out of guesses!");
+
+            }
         }
+        if (userGuess.equals(trueColor)) {
+            System.out.println("Your guess is correct!");
+        }
+
     }
-} //end class Server
+}
+
+
+
+
+
